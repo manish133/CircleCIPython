c2a0d3d2d587c091be4d874eb1e48bca88d660a3
unknown <manishapte133@gmail.com>
2022-04-20 08:14:24 +0530

    Circle Ci Commit Python

---
+++ Main1.py
@@ -0,0 +1,11 @@
+def Add(a,b):
+	return a + b;
+
+def display():
+	print("hello")
+
+if __name__ == '__main__':
+	display()	
+
+
+
---
+++ Test.py
@@ -0,0 +1,8 @@
+from Main1 import Add
+
+def testAdd():
+	assert Add(3,5) == 8
+	print("Add Function working correctly")
+
+if __name__ == '__main__':
+	testAdd()
